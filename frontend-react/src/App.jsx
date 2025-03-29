745a362489e1be33323401c1eac5b48e477ece36
gchin108 <130980531+gchin108@users.noreply.github.com>
2025-03-28 20:01:35 -0700

    registration page and functionality

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,12 +2,22 @@ import "./assets/css/style.css";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 import { Main } from "./components/main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
+
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- frontend-react/src/components/header.jsx
+++ frontend-react/src/components/header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <>
       <nav className="navbar container pt-3 pb-3 align-items-start">
-        <a className="navbar-brand text-light" href="/">
+        <Link className="navbar-brand text-light" to="/">
           Stock Prediction Portal
-        </a>
+        </Link>
         <div>
-          <Button text="Login" class="btn-outline-info" url="/dashboard" />
+          <Button text="Login" class="btn-outline-info" url="/login" />
           &nbsp;
-          <Button class="btn btn-info" text="Register" />
+          <Button class="btn btn-info" text="Register" url="/register" />
         </div>
       </nav>
       ;
